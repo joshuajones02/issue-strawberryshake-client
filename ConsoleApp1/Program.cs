c5a22d12b380e3635ec1c1bd722f66d57ef752ca
joshuajones02 <joshuajones02@live.com>
2022-01-23 21:42:23 -0500

    initial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    using System;
+    using System.Text.Json;
+    using Microsoft.Extensions.DependencyInjection;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection() as IServiceCollection;
+            services.AddConferenceClient();
+            var provider = services.BuildServiceProvider();
+            var client = provider.GetService<IConferenceClient>();
+
+
+            Console.WriteLine("Hello World!");
+            var sessions = client.GetSessions.ExecuteAsync();
+
+            Console.WriteLine(JsonSerializer.Serialize(sessions, new JsonSerializerOptions { WriteIndented = true }));
+        }
+    }
+}
